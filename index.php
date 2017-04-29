37188fedbe3ebbc82040cb37f0669cb9e0dc3d20
dolphin0431 <dolphin0431@gmail.com>
2017-04-29 10:28:13 +0800

    2017/4/29

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>php test</title>
+  </head>
+  <body>
+
+    <?php
+      echo "php script language!";
+     ?>
+
+
+  </body>
+</html>
